09c558dfe0e3614334d7b5a50473fdc82c12c9e7
Leroy Dunn <Leroy.Dunn@investec.co.za>
2017-06-08 16:47:49 +0200

    created scope function and binded views to it

--- Angular-JS-MOOC/Mod2Lab/app/menuController.js
+++ Angular-JS-MOOC/Mod2Lab/app/menuController.js
@@ -2,5 +2,9 @@ app.controller('menuController', [
     '$scope',
     function($scope){
         $scope.model = {title: 'Our Menu'}
+
+        $scope.changeMainDish = function(item){
+            $scope.model.mainDish = item;
+        }
     }
 ])
