5137285ab46a9adafaa1e4762fe8b2186cadabb2
leroy <leroy.c.dunn@gmail.com>
2017-06-17 00:19:17 +0200

    setup for searching functionality

--- Angular-JS-MOOC/Mod3Lab/app/pizzaController.js
+++ Angular-JS-MOOC/Mod3Lab/app/pizzaController.js
@@ -1,6 +1,21 @@
 app.controller('pizzaController', [
     '$scope',
     function ($scope) {
-        $scope.model = { title: 'Pizza Builder' };
+        $scope.model = { 
+            title: 'Pizza Builder',
+            availableToppings: [
+                'Cheese', 
+                'Pepperoni', 
+                'Bacon', 
+                'Pineapple', 
+                'Sausage', 
+                'Ham', 
+                'Chicken', 
+                'Mushrooms', 
+                'Onion', 
+                'Olives', 
+                'Green Peppers'
+            ] 
+        };
     }
 ]);
