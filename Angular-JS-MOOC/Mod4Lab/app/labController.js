66c4bbcb9d1a0c6924d43b12020dfb28e9c5d9fb
Leroy Dunn <Leroy.Dunn@investec.co.za>
2017-06-23 12:45:46 +0200

    list buttons to get and display repo details

--- Angular-JS-MOOC/Mod4Lab/app/labController.js
+++ Angular-JS-MOOC/Mod4Lab/app/labController.js
@@ -8,6 +8,7 @@ app.controller('labController', [
 
         $scope.checkOddNumber = checkOddNumber;
         $scope.getRepos = getRepos;
+        $scope.loadDetail = loadDetail;
 
         function checkOddNumber(input) {
         $scope.model.result = 'Working...';
@@ -42,9 +43,18 @@ app.controller('labController', [
                     $scope.model.repos = response.data;
                 }, function (response) {
                      $scope.model.repos = 'Error: ' + response.data.message;
-                }
-        );
-}
+                })
+        }
+
+        function loadDetail(name) {
+            var url = 'https://api.github.com/repos/angular/' + name;
+            $http.get(url)
+                .then(function (response) {
+                    $scope.model.detail = response.data;
+                }, function (response) {
+                    $scope.model.detail = { error: true, message: 'Error: ' + response.data.message };
+                });
+        }
     
     }
 ]);
