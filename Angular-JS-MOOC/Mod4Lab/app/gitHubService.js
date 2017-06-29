67be560c4d421a23d8172d592c8d6c0ff7a5e474
Leroy Dunn <Leroy.Dunn@investec.co.za>
2017-06-29 17:19:16 +0200

    self assesment lab. Added functionality to search repos

--- Angular-JS-MOOC/Mod4Lab/app/gitHubService.js
+++ Angular-JS-MOOC/Mod4Lab/app/gitHubService.js
@@ -2,9 +2,10 @@ angular.module('gitHubService', ['ngResource']).
         factory('gitHub', [
             '$resource',
             function ($resource) {
-                return $resource('https://api.github.com/:action/angular/:id',
+                return $resource('https://api.github.com/:action/:org/:id',
                     {
                         action: '@action',
+                        org: '@org',
                         id: '@id'
                     },
                     {
@@ -15,7 +16,7 @@ angular.module('gitHubService', ['ngResource']).
                         },
                         getDetail: {
                             method: 'GET',
-                            params: { action: 'repos' }
+                            params: { action: 'repos'}
                         }
                     });
             }
--- Angular-JS-MOOC/Mod4Lab/app/labController.js
+++ Angular-JS-MOOC/Mod4Lab/app/labController.js
@@ -4,6 +4,7 @@ app.controller('labController', [
         $scope.model = {
             number: 0,
             result: 'Result',
+            repo_search_value: ''
         }
 
         $scope.checkOddNumber = checkOddNumber;
@@ -38,13 +39,29 @@ app.controller('labController', [
         }
 
         function getRepos() {
-            $scope.model.repos = github.getAll();
+            $scope.model.detail = null;
+            resetRepoSearchValueIfEmpty();
+            github.getAll({org: $scope.model.repo_search_value}).$promise.then(
+                function(response){
+                    $scope.model.repos = response;
+                }, function(response){
+                    alert('no repos found');
+                    $scope.model.repos = null;
+                }
+            )
         }
 
         function loadDetail(name) {
             $scope.model.detail = null;
-            $scope.model.detail = github.getDetail({id: name});
+            resetRepoSearchValueIfEmpty()
+            github.getDetail({org: $scope.model.repo_search_value, id: name}).$promise.then(function(result){
+                $scope.model.detail = result;
+            })
         }
-    
+
+        function resetRepoSearchValueIfEmpty(){
+            if(!$scope.model.repo_search_value)
+                $scope.model.repo_search_value = 'angular';
+        }
     }
 ]);
