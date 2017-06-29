2ee830d39f27a213fef14ed5dcf7759aa4064fd5
Leroy Dunn <Leroy.Dunn@investec.co.za>
2017-06-29 16:10:39 +0200

    replaced $http requests with $resource factory of http requests which tidies up the code a bit

--- Angular-JS-MOOC/Mod4Lab/app/labController.js
+++ Angular-JS-MOOC/Mod4Lab/app/labController.js
@@ -1,6 +1,6 @@
 app.controller('labController', [
-    '$scope', '$timeout', '$q', '$http',
-    function ($scope, $timeout, $q, $http) {
+    '$scope', '$timeout', '$q', '$http','gitHub',
+    function ($scope, $timeout, $q, $http, github) {
         $scope.model = {
             number: 0,
             result: 'Result',
@@ -38,22 +38,12 @@ app.controller('labController', [
         }
 
         function getRepos() {
-            $http.get('https://api.github.com/orgs/angular/repos')
-                .then(function (response) {
-                    $scope.model.repos = response.data;
-                }, function (response) {
-                     $scope.model.repos = 'Error: ' + response.data.message;
-                })
+            $scope.model.repos = github.getAll();
         }
 
         function loadDetail(name) {
-            var url = 'https://api.github.com/repos/angular/' + name;
-            $http.get(url)
-                .then(function (response) {
-                    $scope.model.detail = response.data;
-                }, function (response) {
-                    $scope.model.detail = { error: true, message: 'Error: ' + response.data.message };
-                });
+            $scope.model.detail = null;
+            $scope.model.detail = github.getDetail({id: name});
         }
     
     }
